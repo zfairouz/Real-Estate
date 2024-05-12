b8ebf9977d844ab733eca458f26f19740cb0b3a1
Fouz20 <zejlifairouz@gmail.com>
2024-05-12 03:04:48 +0200

    adding burger menu

--- scripte.js
+++ scripte.js
@@ -1,3 +1,11 @@
+//NavBar menu
+const humburger = document.getElementById("humburger");
+const navMenu = document.getElementById("nav-menu");
+
+humburger.addEventListener('click', () =>{
+    navMenu.classList.toggle("active");
+})
+
 // Image Slider
 
 const slides = document.querySelectorAll(".slides img");
